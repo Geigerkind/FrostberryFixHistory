1e2f65d9533416c5ae09a3a86880db58261e543b
Mesax <gaminglikeaboss.mesax@gmail.com>
2014-03-20 19:18:47 +0100

    enable love is in the air event on 2014-03-24

---
+++ Development/2014_03_20_enable_love_event.sql
@@ -0,0 +1,3 @@
+-- enable game_event - love is in the air
+-- old start time: '2014-02-10 12:01:00'
+UPDATE `game_event` SET `start_time` = '2014-03-24 12:01:00', `end_time` = '2018-12-31 18:00:00' WHERE `eventEntry` = 8;
