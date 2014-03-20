c970efa5ce3be280e604460e574741e8896b62e5
Tom Lem <momori99@web.de>
2014-03-20 20:30:01 +0100

    Alliance spawns / questrelation / SAIs / questfixxes

--- Development/love_is_in_the_air.sql
+++ Development/love_is_in_the_air.sql
@@ -2,16 +2,26 @@
 -- guidrange 1000000-1010000
 -- Horde
 -- creatures / gameobject correction
+DELETE FROM `gameobject` WHERE `id` = 181086;
 DELETE FROM `gossip_menu_option` WHERE `menu_id` = 10976 AND `id` = 2;
-UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` = 3296; -- guard_generic (Dunno what its necessary for)
+UPDATE `creature_template` SET `ScriptName` = '' WHERE `entry` IN (3296, 68); -- guard_generic (Dunno what its necessary for)
 UPDATE `gameobject_template` SET `AIName` = 'SmartGameObjectAI' WHERE `entry` IN (201778);
-UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (38042, 38043, 38044, 38045);
-UPDATE `creature_template` SET `npcflag` = 3 WHERE `entry` = 38043;
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` IN (38042, 38043, 38044, 38045, 37887, 38041, 38040, 38039);
+UPDATE `creature_template` SET `npcflag` = 3 WHERE `entry` IN (38043, 37887, 38041);
 
 DELETE FROM `creature_template` WHERE `entry` IN (71000);
 INSERT INTO `creature_template` (`entry`, `modelid1`,  `modelid2`,  `modelid3`,  `modelid4`, `name`, `minlevel`, `maxlevel`, `faction_a`, `faction_h`, `npcflag`, `speed_walk`, `speed_run`, `mindmg`, `maxdmg`, `attackpower`, `unit_flags`, `unit_flags2`, `type_flags`, `AIName`) VALUES 
 (71000, 4449, 4449, 4449, 4449, 'Crushing the Crown - Trigger', 60, 60, 35, 35, 0, 0, 0, 1, 1, 1, 4, 0, 1048576, 'SmartAI');
 
+-- Stormwind / Ironforge / Exodar / Darnassus
+DELETE FROM `gameobject` WHERE `guid` IN (27100, 27099, 96539, 42842);
+UPDATE `creature` SET `position_x` = -8869, `position_y` = 638.5, `position_z` = 97.1, `orientation` = 0.83 WHERE `guid` = 69; -- Stormwind
+UPDATE `creature` SET `position_x` = -8867.6, `position_y` = 637.1, `position_z` = 97.1, `orientation` = 0.8 WHERE `guid` = 67; -- Stormwind
+UPDATE `creature` SET `position_x` = -4918.1, `position_y` = -982.3, `position_z` = 503, `orientation` = 2.2 WHERE `guid` = 89; -- Ironforge
+UPDATE `creature` SET `position_x` = -4007, `position_y` = -11844.85, `position_z` = 1.45, `orientation` = 4.7 WHERE `guid` = 162141; -- Exodar
+UPDATE `creature` SET `position_x` = -4005, `position_y` = -11845.3, `position_z` = 1.45, `orientation` = 4.8 WHERE `guid` = 162137; -- Exodar
+UPDATE `creature` SET `position_x` = 9870.9, `position_y` = 2492.5, `position_z` = 1317.2, `orientation` = 5.9 WHERE `guid` = 127009; -- Darnassus
+UPDATE `creature` SET `position_x` = 9871.4, `position_y` = 2494.5, `position_z` = 1317.2, `orientation` = 5.9 WHERE `guid` = 127012; -- Darnassus
 
 -- Orgrimmar / Silvermoon / Thunderbluff / Undercity
 DELETE FROM `gameobject` WHERE `guid` IN (42843,96540, 42844, 27101);
@@ -42,47 +52,75 @@ INSERT INTO `game_event_creature` (`eventEntry`, `guid`) VALUES
 (8, 1000001);
 
 -- quests
-DELETE FROM `game_event_creature_quest` WHERE `id` IN (37172, 38328, 37675, 38043) AND `quest` IN (24536, 24541, 24850, 24576, 24851, 24629, 24615);
-DELETE FROM `creature_questrelation` WHERE `id` IN (37172, 38328, 38043) AND `quest` IN (24536, 24541, 24850, 24576, 24851, 24615); 
+DELETE FROM `game_event_creature_quest` WHERE `id` IN (37172, 38328, 37675, 38043, 37887, 38041, 38066, 38325) AND `quest` IN (24536, 24541, 24850, 24576, 24851, 24629, 24615, 24597, 24609, 24655, 24656, 24848, 24657, 24849);
+DELETE FROM `creature_questrelation` WHERE `id` IN (37172, 38328, 38043, 37887, 38041, 38066, 38325) AND `quest` IN (24536, 24541, 24850, 24576, 24851, 24615, 24597, 24609, 24655, 24656, 24848, 24657, 24849); 
 INSERT INTO `creature_questrelation` VALUES
 (37172, 24536),
 (37172, 24541),
 (37172, 24850),
 (37172, 24576),
 (38043, 24615),
-(38328, 24851);
+(38041, 24609),
+(37887, 24597),
+(38328, 24851),
+(38066, 24655),
+(38066, 24656),
+(38066, 24848),
+(38325, 24657),
+(38325, 24849);
 
-DELETE FROM `creature_involvedrelation` WHERE `id` IN (37172, 38328) AND `quest` IN (24536, 24541, 24576, 24850, 24851);
+DELETE FROM `creature_involvedrelation` WHERE `id` IN (37172, 38328, 38066, 38325) AND `quest` IN (24536, 24541, 24576, 24850, 24851, 24655, 24656, 24804, 24657, 24848, 24849);
 INSERT INTO `creature_involvedrelation` VALUES
 (37172, 24536),
 (37172, 24541),
 (37172, 24576),
 (38328, 24850),
 (38328, 24576),
-(38328, 24851);
+(38328, 24851),
+(38066, 24655),
+(38066, 24656),
+(38066, 24804),
+(38066, 24657),
+(38325, 24848),
+(38325, 24849);
 
 UPDATE `quest_template` SET `PrevQuestId` = 0 WHERE `id` = 24536;
 UPDATE `quest_template` SET `NextQuestId` = 0 WHERE `id` = 24805;
+UPDATE `quest_template` SET `PrevQuestId` = 0 WHERE `id` = 24655;
+UPDATE `quest_template` SET `NextQuestId` = 0 WHERE `id` = 24804;
 UPDATE `quest_template` SET `PrevQuestId` = 24576 WHERE `id` = 28935;
 UPDATE `quest_template` SET `method` = 0 WHERE `id` IN (24849, 24851); -- This quest needs a script
 
 -- SAI
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (37172) AND `id` IN (2,3,4,5);
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (3296) AND `id` IN (1,2);
-DELETE FROM `smart_scripts` WHERE `entryorguid` IN (201778, 201716, 71000, 38042, 38043, 38044);
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (37172, 38066) AND `id` IN (2,3,4,5);
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (3296, 68) AND `id` IN (1);
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (201778, 201716, 71000, 38042, 38043, 38044, 38041, 38040, 37887, 38039);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `event_type`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `target_type`,`target_param1`,`target_param2`, `comment`) VALUES 
 (37172, 0, 2, 19, 100, 0, 24541, 0, 0, 0, 11, 71450, 2, 11, 3296, 50, 'On Questaccept - spellcast'),
 (37172, 0, 3, 19, 100, 0, 24536, 0, 0, 0, 11, 71520, 2, 11, 3296, 50, 'On Questaccept - spellcast'),
 (37172, 0, 4, 62, 100, 0, 10976, 0, 0, 0, 56, 50130, 1, 7, 0, 0, 'On gossip select - give item'),
 (37172, 0, 5, 62, 100, 0, 10976, 0, 0, 0, 72, 0, 0, 0, 0, 0, 'On gossip select - close gossip'),
-(3296, 0, 1, 8, 100, 0, 70192, 1, 1, 1, 33, 37558, 0, 7, 0, 0, 'On spellhit - give questcredit'), -- doesnt work 'cause of their script
+(38066, 0, 2, 19, 100, 0, 24656, 0, 0, 0, 11, 71450, 2, 11, 3296, 50, 'On Questaccept - spellcast'),
+(38066, 0, 3, 62, 100, 0, 10976, 0, 0, 0, 56, 50130, 1, 7, 0, 0, 'On gossip select - give item'),
+(38066, 0, 4, 62, 100, 0, 10976, 0, 0, 0, 72, 0, 0, 0, 0, 0, 'On gossip select - close gossip'),
+(3296, 0, 1, 8, 100, 0, 70192, 1, 1, 1, 33, 37558, 0, 7, 0, 0, 'On spellhit - give questcredit'),
+(68, 0, 1, 8, 100, 0, 70192, 1, 1, 1, 33, 37558, 0, 7, 0, 0, 'On spellhit - give questcredit'),
 (201778, 1, 0, 64, 100, 0, 0, 0, 0, 0, 15, 24541, 0, 7, 0, 0, 'On gossip hello - give questcredit'),
+(201778, 1, 1, 64, 100, 0, 0, 0, 0, 0, 15, 24656, 0, 7, 0, 0, 'On gossip hello - give questcredit'),
 (38042, 0, 0, 62, 100, 0, 10948, 0, 0, 0, 56, 49661, 1, 7, 0, 0, 'On gossip select - give item'),
 (38042, 0, 1, 62, 100, 0, 10948, 0, 0, 0, 72, 0, 0, 0, 0, 0, 'On gossip select - close gossip'),
 (38043, 0, 0, 62, 100, 0, 10948, 0, 0, 0, 56, 49661, 1, 7, 0, 0, 'On gossip select - give item'),
 (38043, 0, 1, 62, 100, 0, 10948, 0, 0, 0, 72, 0, 0, 0, 0, 0, 'On gossip select - close gossip'),
 (38044, 0, 0, 62, 100, 0, 10948, 0, 0, 0, 56, 49661, 1, 7, 0, 0, 'On gossip select - give item'),
-(38044, 0, 1, 62, 100, 0, 10948, 0, 0, 0, 72, 0, 0, 0, 0, 0, 'On gossip select - close gossip');
+(38044, 0, 1, 62, 100, 0, 10948, 0, 0, 0, 72, 0, 0, 0, 0, 0, 'On gossip select - close gossip'),
+(37887, 0, 0, 62, 100, 0, 10948, 0, 0, 0, 56, 49661, 1, 7, 0, 0, 'On gossip select - give item'),
+(37887, 0, 1, 62, 100, 0, 10948, 0, 0, 0, 72, 0, 0, 0, 0, 0, 'On gossip select - close gossip'),
+(38041, 0, 0, 62, 100, 0, 10948, 0, 0, 0, 56, 49661, 1, 7, 0, 0, 'On gossip select - give item'),
+(38041, 0, 1, 62, 100, 0, 10948, 0, 0, 0, 72, 0, 0, 0, 0, 0, 'On gossip select - close gossip'),
+(38040, 0, 0, 62, 100, 0, 10948, 0, 0, 0, 56, 49661, 1, 7, 0, 0, 'On gossip select - give item'),
+(38040, 0, 1, 62, 100, 0, 10948, 0, 0, 0, 72, 0, 0, 0, 0, 0, 'On gossip select - close gossip'),
+(38039, 0, 0, 62, 100, 0, 10948, 0, 0, 0, 56, 49661, 1, 7, 0, 0, 'On gossip select - give item'),
+(38039, 0, 1, 62, 100, 0, 10948, 0, 0, 0, 72, 0, 0, 0, 0, 0, 'On gossip select - close gossip');
 --(71000, 0, 0, 60, 100, 0, 71024, 1, 1, 1, 33, 38035, 0, 18, 100, 0, 'On spellhit - give questcredit'); -- doesnt work cause 71024 is an area effect // need anoter solution
 
 -- conditions
@@ -94,4 +132,5 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`,`SourceGroup`,`SourceEntry`,
 (15,10948,0,0,0,2,0,49661,1,0,1,0,0,'','only show if player has not item'),
 (15,10976,0,0,0,2,0,50130,1,0,1,0,0,'','only show if player has not item'),
 (15,10976,0,0,0,9,0,28935,0,0,0,0,0,'','show if player is on quest'),
+(15,10976,0,0,1,9,0,28934,0,0,0,0,0,'','show if player is on quest'),
 (13,1,71024,0,0,31,0,3,71000,0,0,0,0,'','spell_implecit_target Trigger NPC');
