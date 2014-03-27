9c2950ce9677f3b1f2e95a4d5ab8956669219a81
Tom Lem <momori99@web.de>
2014-03-27 20:03:32 +0100

    startup reference_loot_template issue

---
+++ Development/startup/2014_03_27_startup_reference_loot_template.sql
@@ -0,0 +1,19 @@
+DELETE FROM `reference_loot_template` WHERE `item` IN (52989, 6295, 66537);
+DELETE FROM `reference_loot_template` WHERE `entry` IN (
+13
+,14
+,15
+,16
+,21524
+,47177
+,47497
+,49809
+,56456
+,59502
+,65092
+,65096
+,65119
+,65134
+,67129
+,67425
+,68787);
