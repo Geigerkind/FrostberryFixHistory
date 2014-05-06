1e97d4b980777af77b169d7d0a643cdac5bcf8a3
Tom Lem <momori99@web.de>
2014-05-06 20:33:41 +0200

    auto despawn of bwembas spirit (logging out/port problem)

--- Development/Rise of the Zandalari/2014_05_06_rotz_questchain.sql
+++ Development/Rise of the Zandalari/2014_05_06_rotz_questchain.sql
@@ -12,7 +12,8 @@ INSERT INTO `creature_template_addon` (`entry`, `auras`) VALUES
 
 DELETE FROM `smart_scripts` WHERE `entryorguid` IN (52234);
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`,`target_param1`,`target_param2`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
-(52234, 0, 0, 1, 1, 100, 1, 1, 1, 1, 1, 29, 0, 0, 0, 0, 0, 0, 18, 100, 0, 0, 0, 0, 0, 'On spawn follow player');
+(52234, 0, 0, 0, 1, 100, 1, 1, 1, 1, 1, 29, 0, 0, 0, 0, 0, 0, 18, 100, 0, 0, 0, 0, 0, 'On spawn follow player'),
+(52234, 0, 1, 0, 60, 100, 1, 60000, 60000, 1, 1, 41, 500, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 'On UPDATE completed despawn');
 
 DELETE FROM `spell_area` WHERE `spell` = 96591;
 INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_start_active`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES
