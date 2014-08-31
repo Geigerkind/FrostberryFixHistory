b51a2a508bf08b8d9e5d6fbbeb33ab7783e36201
Mesax <gaminglikeaboss.mesax@gmail.com>
2014-08-31 19:21:09 +0200

    vergessene Questparts hinzugefügt

--- Miscellaneous/Items/Tarecgosa's Rest/2014_07_30_autocomplete.sql
+++ Miscellaneous/Items/Tarecgosa's Rest/2014_07_30_autocomplete.sql
@@ -1,8 +1,10 @@
 -- Horde NPC: 52382
 -- Alliance NPC: 52408
 
-UPDATE `quest_template` SET `method` = 0 WHERE `id` IN (29193, 29194, 29225, 29234, 29239, 29240, 29269);
+UPDATE `quest_template` SET `method` = 0 WHERE `id` IN (29193, 29194, 29225, 29234, 29239, 29240, 29269, 29312, 29309);
 UPDATE `quest_template` SET `method` = 2, `SourceItemId` = 69646 WHERE `id` = 29234;
+UPDATE `quest_template` SET `method` = 2, `RequiredItemId1` = 71084, `RequiredItemCount1` = 1, `RequiredNpcOrGo1` = 0, `RequiredNpcOrGoCount1` = 0 WHERE `id` = 29285;
+
 
 REPLACE INTO `creature_questrelation` VALUES
 (52382, 29193),
@@ -20,7 +22,13 @@ REPLACE INTO `creature_questrelation` VALUES
 (52382, 29269),
 (52408, 29269),
 (52382, 29270),
-(52408, 29270);
+(52408, 29270),
+(52382, 29285),
+(52408, 29285),
+(52382, 29308),
+(52408, 29307),
+(52382, 29309),
+(52408, 29312);
 
 REPLACE INTO `creature_involvedrelation` VALUES
 (52382, 29193),
@@ -36,4 +44,12 @@ REPLACE INTO `creature_involvedrelation` VALUES
 (52382, 29240),
 (52408, 29240),
 (52382, 29269),
-(52408, 29269);
+(52408, 29269),
+(52382, 29270),
+(52408, 29270),
+(52382, 29285),
+(52408, 29285),
+(52382, 29308),
+(52408, 29307),
+(52382, 29309),
+(52408, 29312);
